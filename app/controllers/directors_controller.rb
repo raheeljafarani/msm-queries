74ef36fc55d596ef6d69c97c6cbceb62afd6a6d2
raheeljafarani <rjafaran@chicagobooth.edu>
2025-04-30 01:12:00 +0000

    Eldest and youngest pages built

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -4,6 +4,22 @@ def index
   render({ :template => "director_templates/list"})
 end
 
+def junior
+  @the_youngest_director = Director.maximum( :dob )
+  @name_instance = Director.where({ :dob => @the_youngest_director})
+  @dob_format = @the_youngest_director.strftime("%B %e, %Y")
+  @youngest_name = @name_instance.at(0)
+  render({ :template => "director_templates/youngest"})
+end
+
+def senior
+  @the_eldest_director = Director.minimum( :dob )
+  @name_instance = Director.where({ :dob => @the_eldest_director})
+  @dob_format = @the_eldest_director.strftime("%B %e, %Y")
+  @eldest_name = @name_instance.at(0)
+  render({ :template => "director_templates/eldest"})
+end
+
 def show
   the_id = params.fetch("the_id")
 
